fb07fac77de509b12c21655d47ff8469610929a8
liu <1465638317@qq.com>
2022-12-24 17:51:33 +0800

    滚动tab封装

--- src/components/tab-cards/style.js
+++ src/components/tab-cards/style.js
@@ -2,10 +2,9 @@ import styled from "styled-components";
 
 export const TabCardsWrapper = styled.div`
   box-sizing: border-box;
-  display: flex;
   position: relative;
   width: 100%;
-  padding: 16px 8px;
+  padding: 0 8px;
   margin: 0px -8px;
   .icon {
     cursor: pointer;
@@ -20,6 +19,7 @@ export const TabCardsWrapper = styled.div`
     justify-content: center;
     top: 50%;
     transform: translateY(-50%);
+    z-index: 99;
   }
   .left-icon {
     left: -4px;
@@ -28,33 +28,38 @@ export const TabCardsWrapper = styled.div`
     right: -4px;
   }
   .tabs {
-    display: flex;
-    .word-tab {
-      flex-shrink: 0;
-      flex-basis: 120px; //最小宽度
-      cursor: pointer;
-      height: 48px;
-      line-height: 48px;
-      text-align: center;
-      padding: 0 16px;
-      font-size: 17px;
-      color: ${props => props.theme.fontPrimaryColor};
-      font-weight: bold;
-      border: solid 0.5px #D8D8D8;
-      border-radius: 3px;
-      box-shadow: 0 1px 2px rgba(0, 0, 0, .15);
-      margin-right: 16px;
-      white-space: nowrap;
-      transition: box-shadow 0.4s;
-      &:hover {
-        box-shadow: 0 3px 6px rgba(0, 0, 0, .15);
+    padding: 16px 0;
+    overflow-x: hidden;
+    .tab-scroll{
+      display: flex;
+      transition: all 0.4s;
+      .word-tab {
+        flex-shrink: 0;
+        flex-basis: 120px; //最小宽度
+        cursor: pointer;
+        height: 48px;
+        line-height: 48px;
+        text-align: center;
+        padding: 0 16px;
+        font-size: 17px;
+        color: ${props => props.theme.fontPrimaryColor};
+        font-weight: bold;
+        border: solid 0.5px #D8D8D8;
+        border-radius: 3px;
+        box-shadow: 0 1px 2px rgba(0, 0, 0, .15);
+        margin-right: 16px;
+        white-space: nowrap;
+        transition: box-shadow 0.4s;
+        &:hover {
+          box-shadow: 0 3px 6px rgba(0, 0, 0, .15);
+        }
       }
-    }
-    .active-word-tab {
-      color: #fff;
-      background: #00848A;
-      box-shadow: 0 7px 14px rgba(0, 132, 138, .15);
-      border: solid 0.5px #00848A;
+      .active-word-tab {
+        color: #fff;
+        background: #00848A;
+        box-shadow: 0 7px 14px rgba(0, 132, 138, .15);
+        border: solid 0.5px #00848A;
+      }
     }
   }
 `
--- src/components/tab-cards/tab-cards.js
+++ src/components/tab-cards/tab-cards.js
@@ -1,4 +1,4 @@
-import {memo} from 'react'
+import {memo, useState, useRef, useEffect} from 'react'
 import {TabCardsWrapper} from "./style";
 import PropTypes from "prop-types";
 import IconArrowRight from "@/assets/svg/icon-arrow-right";
@@ -6,18 +6,45 @@ import IconArrowLeft from "@/assets/svg/icon-arrow-left";
 import classNames from "classnames";
 
 const TabCards = memo((props) => {
+  const [showLeft, setShowLeft] = useState(false)
+  const [showRight, setShowRight] = useState(false)
+  const [index, setIndex] = useState(0) //当前在第一个的索引
+  const scrollRef = useRef() //滚动去
+
+  //初始化，如果长度不超出就不显示最右侧按钮
+  useEffect(() => {
+    let width = scrollRef.current.scrollWidth - scrollRef.current.clientWidth
+    if (width > 0) setShowRight(true)
+  }, [props.tabList])
+
+  //每次按钮第一个位置的标签为下一个，索引加1/减1
+  const handelClick = val => {
+    let newIndex = index + val
+    setIndex(newIndex)
+    //拿到下一个标签
+    let newItem = scrollRef.current.children[newIndex]
+    //拿到下一个标签距最左侧的距离,并让父组件滚动
+    scrollRef.current.style.transform = `translate(-${newItem.offsetLeft}px)`
+
+    //判断(所有滚动的长度-左侧滚入的距离)的差是否小于展示区的长度，小于则剩余的部分在展示区能全部展示，需要隐藏右侧按钮
+    let res = (scrollRef.current.scrollWidth - newItem.offsetLeft) <= scrollRef.current.clientWidth
+    setShowRight(!res)
+    setShowLeft(newIndex > 0)
+  }
 
   return (
     <TabCardsWrapper>
-      <div className='icon left-icon'><IconArrowLeft/></div>
-      <div className='icon right-icon'><IconArrowRight/></div>
+      {showLeft ? <div className='icon left-icon' onClick={() => handelClick(-1)}><IconArrowLeft/></div> : null}
+      {showRight ? <div className='icon right-icon' onClick={() => handelClick(+1)}><IconArrowRight/></div> : null}
       <div className='tabs'>
-        {
-          props.tabList.map(item => {
-            return <div className={classNames('word-tab', {'active-word-tab': item === props.activeName})} key={item}
-                        onClick={() => props.changeTab(item)}>{item}</div>
-          })
-        }
+        <div ref={scrollRef} className='tab-scroll'>
+          {
+            props.tabList.map(item => {
+              return <div className={classNames('word-tab', {'active-word-tab': item === props.activeName})} key={item}
+                          onClick={() => props.changeTab(item)}>{item}</div>
+            })
+          }
+        </div>
       </div>
     </TabCardsWrapper>
   )
