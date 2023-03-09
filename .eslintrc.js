04abd81a849194f6cb67a1c049c4e24cc3dd9c92
LiuHN <1465638317@qq.com>
2023-03-09 14:21:07 +0800

    项目结合eslint+prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'google',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {}
+}
